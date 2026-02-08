4cf1089683e6786f5fad7d84410e50ec4651e7b1
TrHlow <thlong190705@gmail.com>
2026-02-08 20:20:15 +0700

    feat: protect apis and user context from jwt

---
+++ EduCycle.Api/Controllers/CategoriesController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/categories")]
+[Authorize]   // 🔒 THÊM DÒNG NÀY
+public class CategoriesController : ControllerBase
+{
+
+}
