a898a63181ae5b9ff9555dad7410d627cf9ba599
TrHlow <thlong190705@gmail.com>
2026-02-15 13:16:17 +0700

    feat(migration): add AdminApprovalAndFrontendSync migration

---
+++ EduCycle.Api/Migrations/20260214134606_FixSeedDataStaticHash.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EduCycle.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixSeedDataStaticHash : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000001"),
+                column: "PasswordHash",
+                value: "$2a$11$8bHsTyPgE.P/CB7rCNvgbeW5/Nw5iH1RwaOkKogQLdJAnlFAGAyaa");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000001"),
+                column: "PasswordHash",
+                value: "$2a$11$Q.dlc4NyfjDilVOVb.YPDuABOiwGAunoMHBYm0bIb8GQ2T7.p3u3W");
+        }
+    }
+}
