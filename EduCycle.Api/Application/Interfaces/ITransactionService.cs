a5364a6815bcbc7819f241c14b781a0cf1a9370b
TrHlow <thlong190705@gmail.com>
2026-02-15 13:16:07 +0700

    feat(app): add admin approval, OTP, messages, enriched responses, update validators for frontend

--- EduCycle.Api/Application/Interfaces/ITransactionService.cs
+++ EduCycle.Api/Application/Interfaces/ITransactionService.cs
@@ -7,5 +7,9 @@ public interface ITransactionService
     Task<TransactionResponse> CreateAsync(CreateTransactionRequest request, Guid buyerId);
     Task<TransactionResponse> GetByIdAsync(Guid id);
     Task<List<TransactionResponse>> GetAllAsync();
+    Task<List<TransactionResponse>> GetMyTransactionsAsync(Guid userId);
     Task<TransactionResponse> UpdateStatusAsync(Guid id, UpdateTransactionStatusRequest request);
+    Task<object> GenerateOtpAsync(Guid id);
+    Task VerifyOtpAsync(Guid id, string otp);
+    Task<TransactionResponse> ConfirmReceiptAsync(Guid id);
 }
