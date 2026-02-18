ecbf75772c0610b7955820f53d1d899d26daff45
TrHlow <thlong190705@gmail.com>
2026-02-18 13:57:26 +0700

    fix: add migration for User Phone/PhoneVerified fields

---
+++ EduCycle.Api/Migrations/20260218065603_AddPhoneToUser.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EduCycle.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPhoneToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Phone",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "PhoneVerified",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000001"),
+                columns: new[] { "Phone", "PhoneVerified" },
+                values: new object[] { null, false });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Phone",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "PhoneVerified",
+                table: "Users");
+        }
+    }
+}
