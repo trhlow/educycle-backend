90fe5f959c4a71d412bf26af0f64fffc2d4ff772
TrHlow <thlong190705@gmail.com>
2026-02-18 14:34:05 +0700

    feat: backend for social login, user reviews, auto-delete, masking

---
+++ EduCycle.Api/Migrations/20260218073024_AddUserReviews.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EduCycle.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserReviews : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ProductId",
+                table: "Reviews",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "TargetUserId",
+                table: "Reviews",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "TransactionId",
+                table: "Reviews",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_TargetUserId",
+                table: "Reviews",
+                column: "TargetUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reviews_Users_TargetUserId",
+                table: "Reviews",
+                column: "TargetUserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reviews_Users_TargetUserId",
+                table: "Reviews");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Reviews_TargetUserId",
+                table: "Reviews");
+
+            migrationBuilder.DropColumn(
+                name: "TargetUserId",
+                table: "Reviews");
+
+            migrationBuilder.DropColumn(
+                name: "TransactionId",
+                table: "Reviews");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ProductId",
+                table: "Reviews",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+        }
+    }
+}
